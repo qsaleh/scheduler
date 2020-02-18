20ee9c7c433645e640512ff4aac43e2bd882b434
Qusai Saleh <qusaisaleh@Candices-MacBook-Pro.local>
2020-02-18 15:02:40 -0700

    added functionality

--- src/components/Appointment/index.js
+++ src/components/Appointment/index.js
@@ -2,11 +2,16 @@ import React from "react";
 
 import "components/Appointment/styles.scss";
 
-import "components/Appointment/Header";
-import "components/Appointment/Show";
-import "components/Appointment/Empty";
+import Header from "components/Appointment/Header";
+import Show from "components/Appointment/Show";
+import Empty from "components/Appointment/Empty";
 
-export default function Appointment(){
-  return <article className="appointment"></article>
+export default function Appointment(props){
+  return (
+  <article className="appointment">
+  <Header time={props.time} />
+  {props.interview && <Show/>}<Empty/>
+  </article>
+  )
 }
 
